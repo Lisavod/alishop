25a6f9103a6c9ef558d821ada7147d3d7de4dd20
vn-lisa <lisavod@gmail.com>
2022-09-16 11:48:09 +0200

    adding persist lib logic -> store the states into the localstorage

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 import './index.scss';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react'; //special Component from persist lib
 // import { UserProvider } from './contexts/user.contex';
 // import { CategoriesProvider } from './contexts/categories.context';
 // import { DropdownProvider } from './contexts/dropdown.context';
@@ -14,17 +15,20 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render( 
     <React.StrictMode>
-        <Provider store={store}>
-            <BrowserRouter>
-                {/* <UserProvider>
-                    <CategoriesProvider>
-                        <DropdownProvider> */}
-                        <App />
-                        {/* </DropdownProvider>
-                    </CategoriesProvider>
-                </UserProvider> */}
-            </BrowserRouter> 
-        </Provider>
+        <Provider store = {store}>
+            <PersistGate loading={null} persistor={persistor}> {/* lading null => it will render nothing until get data from localStorage*/}
+                <BrowserRouter> {
+        /* <UserProvider>
+                            <CategoriesProvider>
+                                <DropdownProvider> */
+    } 
+                    <App /> {
+        /* </DropdownProvider>
+                            </CategoriesProvider>
+                        </UserProvider> */
+    } 
+                </BrowserRouter> 
+             </PersistGate> 
+        </Provider> 
     </React.StrictMode>
 );
-
--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,26 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
-
-
+import { persistStore, persistReducer } from 'redux-persist'; //persist any users state to localStorage
+import storage from 'redux-persist/lib/storage'; //by default in all browsers will use local storage
 //root-reducer
 import { rootReducer } from './root-reducer';
+
+
+const persistConfig = {
+    key: 'root', // 'root' tells that we want to persist everything
+    storage,
+    blacklist: ['user'] //specify reducer we don't want to persist
+};
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [logger];
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, composedEnhancers);
+// export const store = createStore(rootReducer, composedEnhancers);
+export const store = createStore(persistedReducer, composedEnhancers);
+
+//
+export const persistor = persistStore(store);
+//in index.js we import the component where we pass that
